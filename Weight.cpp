636bc12dde1a604fbfdbe37b103c4295f96209c7
Christian <lichrist@hawaii.edu>
2022-04-15 09:08:41 -1000

    First Commits

---
+++ Weight.cpp
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Christian Li <lichrist@hawaii.edu>
+/// @date   15_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Weight.h"
---
+++ Weight.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.h
+/// @version 1.0
+///
+/// @author Christian Li <lichrist@hawaii.edu>
+/// @date   15_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef EE205_LAB12A_FATCAT_WEIGHT_H
+#define EE205_LAB12A_FATCAT_WEIGHT_H
+
+
+class Weight {
+
+};
+
+
+#endif //EE205_LAB12A_FATCAT_WEIGHT_H
