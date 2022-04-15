86936f294f1e07828bc7bb92c9c3c89029f23287
Christian <lichrist@hawaii.edu>
2022-04-15 10:19:17 -1000

    Added enums/constructors

--- Weight.h
+++ Weight.h
@@ -8,12 +8,41 @@
 /// @author Christian Li <lichrist@hawaii.edu>
 /// @date   15_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
+#pragma once
+#include <string>
 
 #ifndef EE205_LAB12A_FATCAT_WEIGHT_H
 #define EE205_LAB12A_FATCAT_WEIGHT_H
 
 
 class Weight {
+
+public:
+//////////////Enumerations/////////////////
+    enum UnitOfWeight { POUND, KILO, SLUG};
+
+    static const float UNKNOWN_WEIGHT;
+    static const float KILOS_IN_A_POUND;
+    static const float SLUGS_IN_A_POUND;
+
+    static const std::string POUND_LABEL;
+    static const std::string KILO_LABEL;
+    static const std::string SLUG_LABEL;
+
+public:
+    float getWeight() const noexcept;
+    float getMaxWeight() const noexcept;
+    UnitOfWeight getUnitOfWeight() const noexcept;
+
+private:
+    float weight;
+    float maxWeight;
+    enum UnitOfWeight unitOfWeight;
+
+public:
+//////////////Constructors/////////////////
+    Weight() noexcept;
+    Weight(float newWeight);
 
 };
 
